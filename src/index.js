4aa28cc7ea2945de06245153106df6f6babcb82b
Btu1997 <bittumishra08223@gmail.com>
2022-08-25 21:09:08 +0530

    global url

--- src/index.js
+++ src/index.js
@@ -14,12 +14,28 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+// app.use (
+//     function (req, res, next) {
+//         console.log ("inside GLOBAL MW");
+//         next();
+//   }
+//   );
+
+const moment= require('moment')
+const time=moment();
+app.use(
+    function(req,res,next){
+        console.log("this is global middleware")
+        console.log(time.format("yyyy-mm-dd"))
+        console.log(time.format("hh-mm-ss"))
+        //console.log(req.socket.remoteAddress)
+        console.log(req.ip)
+        console.log(req.originalUrl)
+        next()
+        
+    }
+)
+
 
 app.use('/', route);
 
